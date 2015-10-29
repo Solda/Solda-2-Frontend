d757f28e75bd2c018ba5f7d06d220f78685289fa
Leo <leolin@me.com>
2015-10-29 18:01:23 +0800

    搜尋業面

--- build/js/slide.js
+++ build/js/slide.js
@@ -1,4 +1,7 @@
 $( document ).ready(function() {
+		
+		// 首頁簡單輪播
+
 		$("#slideshow > div:gt(0)").hide();
 		
 		setInterval(function() { 
@@ -10,8 +13,21 @@ $( document ).ready(function() {
 		    .appendTo('#slideshow');
 		},  5000);
 		
+		// 縮放目錄
+
 		$( ".show-all-category" ).click(function() {
 			$( ".category-container" ).toggleClass( 'show-all' );
 		});		
 		
+		// 顯示搜尋
+		
+		$( "a.category-icon.search" ).click(function() {
+			$( "main.mdl-layout__content" ).toggleClass( 'show-search-full' );
+			$("#search").focus();
+		});		
+		$( ".logo img" ).click(function() {
+			$( "main.mdl-layout__content" ).toggleClass( 'show-search-full' );
+		});		
+
+
 });
