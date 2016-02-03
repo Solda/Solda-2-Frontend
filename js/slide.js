c448cdf50783905858c4fb88c0946aed5ab7f349
Leo <leolin@me.com>
2016-02-03 16:46:16 +0800

    要做一個 sidebar 出來，跟 mdl 一樣

--- js/slide.js
+++ js/slide.js
@@ -1,24 +1,5 @@
 $( document ).ready(function() {
 		
-		// 首頁簡單輪播
-
-		$("#slideshow > div:gt(0)").hide();
-		
-		setInterval(function() { 
-		  $('#slideshow > div:first')
-		    .fadeOut(1000)
-		    .next()
-		    .fadeIn(1000)
-		    .end()
-		    .appendTo('#slideshow');
-		},  5000);
-		
-		// 縮放目錄
-
-		$( ".show-all-category" ).click(function() {
-			$( ".category-container" ).toggleClass( 'show-all' );
-		});		
-		
 		// 商家頁面，點預告可展開
 
 		$( "a.expand-more, a.expand-less" ).click(function() {
@@ -61,5 +42,15 @@ $( document ).ready(function() {
 			$("#user-nav").hide();
 			$("#store-nav").show();
 		});
+
+		// 首頁 topbar 切換 search bar
+
+		$(".search-field").hide();
+		$(".show-search-field").click(function(){
+			$(".hide-when-search").hide()
+			$("#sample1").focus();
+			$(".search-field").show();
+		});
+
 
 });
