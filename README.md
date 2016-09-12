80f8c5a0d3c3d3c5470bbe9d053b4f112ea8df14
Leo <leolin@me.com>
2016-09-12 15:49:30 +0800

    Add Fire.app for David.

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
-# 主要頁面
+# Solda Frontend
 
-- [首頁](http://solda.github.io/Solda-2-Frontend/build/index.html)
-- [通用產品列表](http://solda.github.io/Solda-2-Frontend/build/category.html)
-- [搜尋](http://solda.github.io/Solda-2-Frontend/build/search.html)
+0. 我使用的是 `gh-pages` branch
+1. 解壓內附的 `Fire.app`
+2. 執行後 Watch 目錄
+3. 打開 http://localhost:24681/ 看檔案
+4. 若需要看 html 則直接看 `build` 資料夾，不需要用 Fire.app
--- js/slide.js
+++ js/slide.js
@@ -31,4 +31,103 @@ $( document ).ready(function() {
 			$("#store-nav").show();
 		});
 
+		// newproduct.html
+
+var shipping1Showed = false;
+$( ".shipping1.card-padding .mdl-checkbox__label" ).click(function (e) {
+	if (!shipping1Showed) {
+		$(".shipping1.shipping-box").slideDown();
+	} else {
+		$(".shipping1.shipping-box").slideUp();
+	}
+	shipping1Showed = !shipping1Showed;
+});
+
+var shipping2Showed = false;
+$( ".shipping2.card-padding .mdl-checkbox__label" ).click(function (e) {
+	if (!shipping2Showed) {
+		$(".shipping2.shipping-box").slideDown();
+	} else {
+		$(".shipping2.shipping-box").slideUp();
+	}
+	shipping2Showed = !shipping2Showed;
+});
+var shipping3Showed = false;
+$( ".shipping3.card-padding .mdl-checkbox__label" ).click(function (e) {
+	if (!shipping3Showed) {
+		$(".shipping3.shipping-box").slideDown();
+	} else {
+		$(".shipping3.shipping-box").slideUp();
+	}
+	shipping3Showed = !shipping3Showed;
+});
+var shipping4Showed = false;
+$( ".shipping4.card-padding .mdl-checkbox__label" ).click(function (e) {
+	if (!shipping4Showed) {
+		$(".shipping4.shipping-box").slideDown();
+	} else {
+		$(".shipping4.shipping-box").slideUp();
+	}
+	shipping4Showed = !shipping4Showed;
+});
+var shipping5Showed = false;
+$( ".shipping5.card-padding .mdl-checkbox__label" ).click(function (e) {
+	if (!shipping5Showed) {
+		$(".shipping5.shipping-box").slideDown();
+	} else {
+		$(".shipping5.shipping-box").slideUp();
+	}
+	shipping5Showed = !shipping5Showed;
+});
+var shipping6Showed = false;
+$( ".shipping6.card-padding .mdl-checkbox__label" ).click(function (e) {
+	if (!shipping6Showed) {
+		$(".shipping6.shipping-box").slideDown();
+	} else {
+		$(".shipping6.shipping-box").slideUp();
+	}
+	shipping6Showed = !shipping6Showed;
+});
+var shipping7Showed = false;
+$( ".shipping7.card-padding .mdl-checkbox__label" ).click(function (e) {
+	if (!shipping7Showed) {
+		$(".shipping7.shipping-box").slideDown();
+	} else {
+		$(".shipping7.shipping-box").slideUp();
+	}
+	shipping7Showed = !shipping7Showed;
+});
+var shipping8Showed = false;
+$( ".shipping8.card-padding .mdl-checkbox__label" ).click(function (e) {
+	if (!shipping8Showed) {
+		$(".shipping8.shipping-box").slideDown();
+	} else {
+		$(".shipping8.shipping-box").slideUp();
+	}
+	shipping8Showed = !shipping8Showed;
+});
+
+var misc = false;
+$( "a.show-more-info" ).click(function (e) {
+	if (!misc) {
+		$(".more-info").slideDown();
+	} else {
+		$(".more-info").slideUp();
+	}
+	misc = !misc;
+});
+
+var independent = false;
+$( ".mdl-radio__label.show-independent-shipping" ).click(function (e) {
+	if (!independent) {
+		$(".independent-shipping").slideDown();
+	} else {
+		$(".independent-shipping").slideUp();
+	}
+	independent = !independent;
+});
+
+
+
+
 });
